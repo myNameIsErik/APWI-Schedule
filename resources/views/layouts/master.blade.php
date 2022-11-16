b1e9debaf0ef4992bb5b3c7bd575b71d3bce9255
erik_s09 <septianaerik45@gmail.com>
2022-11-16 20:38:38 +0700

    add meta keyword & desc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keyword" content="Aplikasi E-Schedule PPSDM Regional Bandung">
+    <meta name="description" content="Aplikasi E-Schedule PPSDM Regional Bandung, E-Schedule Widya Iswara, E-Schedule WI">
     <title>APWI-Schedule</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/logo/logo.png">
